268666344831103ecb9413e8c584f71260e4dd77
Tianyu Liu <liu.tianyu93@hotmail.com>
2017-03-15 01:10:54 +0100

    #Add basic parser 1. Add parser to parse XML 2. Add test code 3. Add transfer to Map function

--- 2IMW20-Project/Parser.cs
+++ 2IMW20-Project/Parser.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace _2IMW20_Project
 {
-    class Parser
+    class Parser : XmlDocument
     {
+        private string location;
+        private int nodeQuantity;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="location">the file location in URL</param>
+        public Parser(string location) : base()
+        {
+            this.location = location;
+        }
+
+        /// <summary>
+        /// Load the xml file using URL stored in this instance
+        /// </summary>
+        public void Load()
+        {
+            base.Load(this.location);
+        }
+
+        public Dictionary<int, string> GetNodesToMap(string name)
+        {
+            Dictionary<int, string> tempMap = new Dictionary<int, string>();
+            XmlNodeList xmlNodes = base.GetElementsByTagName(name);
+            this.nodeQuantity = xmlNodes.Count;
+            int counter = 0;
+            foreach(XmlNode element in xmlNodes)
+            {
+              
+                if (! (element == null))
+                {
+                    tempMap.Add(counter ++, element.InnerText);
+                }
+                //nodes.Add(element.InnerText);
+                //Console.WriteLine(element.InnerText);
+            }
+
+            return tempMap;
+        }
     }
 }
--- 2IMW20-Project/Program.cs
+++ 2IMW20-Project/Program.cs
@@ -11,42 +11,56 @@ namespace _2IMW20_Project
     {
         static void Main(string[] args)
         {
-            bool exit = false;
-            while (!exit)
+            //bool exit = false;
+            //while (!exit)
+            //{
+            //    bool hasDataset = false;
+            //    while (!hasDataset)
+            //    {
+            //        Console.WriteLine("Please specify a dataset:");
+            //        string datasetPath = Console.ReadLine();
+
+            //        if (datasetPath != "") // TODO: Check whether dataset exists & is valid
+            //            hasDataset = true;
+            //        else
+            //            Console.WriteLine("Invalid path!");
+            //    }
+
+
+            //    // Parse data
+
+            //    // Execute algorithms:
+
+            //    Console.WriteLine("Executing Degree Based...");
+            //    Console.WriteLine("Completed Degree Based");
+
+            //    Console.WriteLine("Executing TRPW...");
+            //    Console.WriteLine("Completed TRPW");
+
+
+            //    // Write results
+
+
+
+            //    Console.WriteLine("Do you wish to try another dataset? (Y/N)");
+
+            //    if (Console.ReadLine() == "N")
+            //        exit = true;
+            //}
+
+            //Check parser
+            string location = "file:///D:/ebay.xml";
+
+            Parser parser = new Parser(location);
+            parser.Load();
+            Dictionary<int,string> testList = parser.GetNodesToMap("seller_name");
+            for (int i = 0; i < testList.Count; i ++)
             {
-                bool hasDataset = false;
-                while (!hasDataset)
-                {
-                    Console.WriteLine("Please specify a dataset:");
-                    string datasetPath = Console.ReadLine();
-
-                    if (datasetPath != "") // TODO: Check whether dataset exists & is valid
-                        hasDataset = true;
-                    else
-                        Console.WriteLine("Invalid path!");
-                }
-
-
-                // Parse data
-
-                // Execute algorithms:
-
-                Console.WriteLine("Executing Degree Based...");
-                Console.WriteLine("Completed Degree Based");
-
-                Console.WriteLine("Executing TRPW...");
-                Console.WriteLine("Completed TRPW");
-
-
-                // Write results
-
-
-
-                Console.WriteLine("Do you wish to try another dataset? (Y/N)");
-
-                if (Console.ReadLine() == "N")
-                    exit = true;
+                string value = "";
+                testList.TryGetValue(i, out value);
+                Console.WriteLine(value);
             }
+            Console.ReadKey();
         }
     }
 }
